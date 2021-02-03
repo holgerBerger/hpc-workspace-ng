81c292a1442a51888b0e6ad5c5a2ad6d01bc42fb
Holger Berger <holger.berger@googlemail.com>
2021-02-04 00:26:50 +0100

    interface DBentry printing entry

--- ws_common/db.d
+++ ws_common/db.d
@@ -10,9 +10,11 @@ import std.path;
 import std.algorithm;
 import std.array;
 import std.string;
+import std.conv;
 import config;
 import dyaml;
 import yamlhelper;
+import core.stdc.time;
 
 
 // interface to access the database
@@ -27,31 +29,87 @@ interface Database {
 	// ...	
 }
 
+// interface for DBentry, allows printing and access for sorting
+interface DBEntry {
+	void print(bool verbose, bool terse);
+	long getRemaining();
+	string getId();
+	long getCreation();
+}
+
 
 // dbentry
 //  struct or assoc array?
 //  if struct we might need a version field
 //  assoc would be very flexible and match the YAML model well, easy to extend
 //  and would carry unknown fields simply over. mix may be?
-class DBEntry {
+class DBEntryV1 : DBEntry {
 	int	dbversion;		// version
-	long 	expiration;		// epoch time of expiration
+	string	id;			// ID of this workspace
+	string	filesystem;		// location
 	string 	workspace;		// directory path
-	int 	extensions;		// extensions, counting down
+	long	creation;		// epoch time of creation
+	long 	expiration;		// epoch time of expiration
+	long 	released;		// epoch time of manual release
 	long	reminder;		// epoch time of reminder to be sent out
+	int 	extensions;		// extensions, counting down
 	string	mailaddress;		// address for reminder email
 	string 	comment;		// some user defined comment
 
 	// read db entry from yaml file
-	void readFromfile(string filename) {
+	void readFromfile(string id, string filesystem, string filename) {
 		auto root = Loader.fromFile(filename).load();
 		dbversion = readValue!int(root, "dbversion", 0); 	// 0 = legacy
+		this.id = id;
+		this.filesystem = filesystem;
+		creation = readValue!long(root, "creation", 0); 	
+		released = readValue!long(root, "released", 0); 	
 		expiration = readValue!long(root, "expiration", 0); 	
+		reminder = readValue!long(root, "reminder", 0); 	
 		workspace = readValue!string(root, "workspace", ""); 	
 		extensions = readValue!int(root, "extensions", 0); 	
-		reminder = readValue!long(root, "reminder", 0); 	
 		mailaddress = readValue!string(root, "mailaddress", ""); 	
 		comment = readValue!string(root, "comment", ""); 	
+	}
+
+	long getRemaining() {
+		long remaining = expiration - time(cast(long *)0L);
+		return remaining;
+	}
+	long getCreation() {
+		return creation;
+	}
+	string getId() {
+		return id;
+	}
+
+	void print(bool verbose, bool terse) {
+		string repr;
+		long remaining = expiration - time(cast(long *)0L);
+
+		stdout.writefln(
+			"Id: %s\n" ~   
+			"    workspace directory  : %s", 
+			id, workspace);	
+		if (remaining<0) {
+			stdout.writefln("    remaining time       : %s", "expired");
+		} else {
+			stdout.writefln("    remaining time       : %d days, %d hours", remaining/(24*3600), (remaining%(24*3600))/3600);
+		}
+		if(!terse) {
+			if(comment!="")
+				stdout.writefln("    comment              : %s", comment);
+			if (creation>0) 
+				stdout.writef("    creation time        : %s", ctime(&creation).to!string );
+			stdout.writef("    expiration time      : %s", ctime(&expiration).to!string );
+			stdout.writefln("    filesystem name      : %s", filesystem);
+		}	
+		stdout.writefln("    available extensions : %d", extensions);
+		if (verbose) {
+			long rd = expiration - reminder/(24*3600);
+			stdout.writef("    reminder             : %s", ctime(&rd).to!string );
+			stdout.writefln("    mailaddress          : %s", mailaddress);
+		}
 	}
 }
 
@@ -106,14 +164,14 @@ public:
 	}
 
 	// read DBentry and return it
-	DBEntry readEntry(string filesystem, string user, string id, bool deleted) {
-		auto entry = new DBEntry;
+	DBEntryV1 readEntry(string filesystem, string user, string id, bool deleted) {
+		auto entry = new DBEntryV1;
 		string filename;
 		if (deleted) 
 			filename = buildPath(config.database(filesystem), config.deleted(filesystem), user~"-"~id);
 		else 
 			filename = buildPath(config.database(filesystem), user~"-"~id);
-		entry.readFromfile(filename);
+		entry.readFromfile(id, filesystem, filename);
 		return entry;
 	}
 }
